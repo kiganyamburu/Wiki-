f7a59be704756e79a1fb063eda17f7be1c14dff0
kiganyamburu <mburukiganya@gmail.com>
2024-12-05 15:25:33 +0300

    about

--- wiki/wiki/urls.py
+++ wiki/wiki/urls.py
@@ -20,5 +20,6 @@ from encyclopedia import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path('/encyclopedia/templates/encyclopedia/about.html', views.about, name="about")
 ]
