52559cb91fd033fd2d986376e117b660ecddaa4b
kiganyamburu <mburukiganya@gmail.com>
2024-12-05 20:19:09 +0300

    new db

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -76,12 +76,14 @@ WSGI_APPLICATION = 'wiki.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wiki',
+        'USER': 'root',
+        'PASSWORD': 'think_and_grow_rich!',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
